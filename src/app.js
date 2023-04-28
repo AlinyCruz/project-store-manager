20a5cba4502fe397f00925c26c794badf178bf2e
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-04-28 15:42:22 -0300

    Requisito 06 em andamento

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const productsController = require('./controllers/productsControllers');
+const salesController = require('./controllers/salesControllers');
+const salesMiddle = require('./middlewares/salesMiddlewares');
 
 const app = express();
 app.use(express.json());
@@ -14,6 +16,8 @@ app.get('/products', productsController.listProducts);
 app.get('/products/:id', productsController.listProductId);
 
 app.post('/products', productsController.createNewProduct);
+
+app.post('/sales', salesMiddle, salesController.createNewSales);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
